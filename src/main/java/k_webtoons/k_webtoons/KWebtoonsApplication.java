0940f774c53c92e6b6683b4d8b70d72ee6ec45fd
ta1355 <eorbs255@naver.com>
2025-03-20 12:39:53 +0900

    2 feat 웹툰 인기순 이름 등 검색 api 구현 (#6)
    * feat: 조회수 높은 웹툰 리스트, 웹툰 이름으로 검색 기능 추가
    
    * feat: 스웨거 문서 추가
    
    ---------
    
    Co-authored-by: mawoo <hanjutun@gmail.com>

--- src/main/java/k_webtoons/k_webtoons/KWebtoonsApplication.java
+++ src/main/java/k_webtoons/k_webtoons/KWebtoonsApplication.java
@@ -1,13 +1,23 @@
 package k_webtoons.k_webtoons;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "큐레이툰",
+                version = "1.0.0",
+                description = "웹툰 추천 서비스"
+        )
+)
 @SpringBootApplication
 public class KWebtoonsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KWebtoonsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KWebtoonsApplication.class, args);
+    }
 
 }
