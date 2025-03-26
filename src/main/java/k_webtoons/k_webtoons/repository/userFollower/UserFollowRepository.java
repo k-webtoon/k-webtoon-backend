26b7950a3c31ebcda7de7d2b0e574ff596851dd1
SoWon <fkdla1024@naver.com>
2025-03-26 11:36:06 +0900

    feat : 팔로워 팔로잉 기능이 추가가 돼.        DB도 추가하게 돼.        Appuser도 설정 추가하게 돼.

---
+++ src/main/java/k_webtoons/k_webtoons/repository/userFollower/UserFollowRepository.java
@@ -0,0 +1,17 @@
+package k_webtoons.k_webtoons.repository.userFollower;
+import k_webtoons.k_webtoons.model.user.AppUser;
+import k_webtoons.k_webtoons.model.user_follow.UserFollow;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserFollowRepository extends JpaRepository<UserFollow, Long> {
+    boolean existsByFollowerAndFollowee(AppUser follower, AppUser followee);
+    Optional<UserFollow> findByFollowerAndFollowee(AppUser follower, AppUser followee);
+    List<UserFollow> findByFollower(AppUser follower);
+    List<UserFollow> findByFollowee(AppUser followee);
+    void deleteByFollowerAndFollowee(AppUser follower, AppUser followee);
+    long countByFollowee(AppUser followee);
+    long countByFollower(AppUser follower);
+}
