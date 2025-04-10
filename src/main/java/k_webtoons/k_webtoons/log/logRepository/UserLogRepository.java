afd5e9492b597c5e0bd460d1000d27d8a14f6051
saron-park <anemone2805@gmail.com>
2025-04-10 14:56:27 +0900

    Log 구현시 삭제 안한거 삭제

--- src/main/java/k_webtoons/k_webtoons/log/logRepository/UserLogRepository.java
+++
@@ -1,7 +0,0 @@
-package k_webtoons.k_webtoons.log.logRepository;
-
-import k_webtoons.k_webtoons.log.logModel.UserLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserLogRepository extends JpaRepository<UserLog , Long> {
-}
